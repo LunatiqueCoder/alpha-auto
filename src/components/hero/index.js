9cf30cb122e4a85f40db53e3cebd56c9c1258315
Alexandru Andronache <alexandru.andronache@iquestgroup.com>
2020-07-01 18:22:14 +0300

    alpha improvements - bucket#4

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -10,25 +10,30 @@ class SimpleSlider extends Component {
       <section id="hero" className="hero hero-static-image">
           <div className="container">
               <div className="row">
-                  <div className="col col-lg-6 col-md-7 slide-caption">
+                  <div className="col col-lg-9 col-md-7 slide-caption large-screen-only">
                     <div className="slide-title">
-                        <h2>Nu contează ce este în spatele tău!</h2>
-                    </div>
-                    <div className="slide-subtitle">
-                        <h3 className="quote-by">- Enzo Ferrari</h3>
-                    </div>
-                    <div className="large-screen-only btns">
-                      <AnchorLink offset='27' href='#contact' className="theme-btn-s2">Contactează-ne</AnchorLink>
-                    </div>
-                    <div className="mobile-buttons-container">
-                      <div className="mobile-only localisation-button">
-                        <AnchorLink offset='20' href='#contact' className="theme-btn-s2">Contact</AnchorLink>
-                      </div>
-                      <div className="mobile-only call-now-button">
-                        <AnchorLink offset='10' href='#appointment' className="theme-btn-s2">Programări</AnchorLink>
-                      </div>
+                        <h3>Indiferent de calatorie, masina a fost mereu alaturi de tine.</h3>
+                      <h3>Respect-o si pregateste-o pentru noi aventuri la <span className="hashtag-alpha-auto">#AlphaAuto</span></h3>
                     </div>
                   </div>
+                <div className="col col-lg-12 col-md-12">
+                  <div className="desktop-buttons-container">
+                    <div className="large-screen-only desktop-button btns">
+                      <AnchorLink offset='27' href='#contact' className="theme-btn-s2">Contactează-ne</AnchorLink>
+                    </div>
+                    <div className="large-screen-only desktop-button btns">
+                      <AnchorLink offset='27' href='#appointment' className="theme-btn-s2">Programări</AnchorLink>
+                    </div>
+                  </div>
+                  <div className="mobile-buttons-container">
+                    <div className="mobile-only localisation-button">
+                      <AnchorLink offset='20' href='#contact' className="theme-btn-s2">Contact</AnchorLink>
+                    </div>
+                    <div className="mobile-only call-now-button">
+                      <AnchorLink offset='10' href='#appointment' className="theme-btn-s2">Programări</AnchorLink>
+                    </div>
+                  </div>
+                </div>
               </div>
           </div>
       </section>
