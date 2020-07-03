cfb7faf95ce3bb18a00a112b6e6a06277e50d5da
Alexandru Andronache <alexandru.andronache@iquestgroup.com>
2020-07-03 19:32:48 +0300

    alpha improvements - update icons + SEO

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -9,13 +9,14 @@ class SimpleSlider extends Component {
     return (
       <section id="hero" className="hero hero-static-image">
           <div className="container">
-              <div className="row">
-                  <div className="col col-lg-9 col-md-7 slide-caption large-screen-only">
-                    <div className="slide-title">
-                        <h3>Indiferent de calatorie, masina a fost mereu alaturi de tine.</h3>
-                      <h3>Respect-o si pregateste-o pentru noi aventuri la <span className="hashtag-alpha-auto">#AlphaAuto</span></h3>
-                    </div>
+            <div className="row hero-container large-screen-only">
+                <div className="col col-lg-9 col-md-7 slide-caption large-screen-only">
+                  <div className="slide-title large-screen-only">
+                    <h1 className="h1-heading">Alpha Auto</h1>
+                    <h3>Indiferent de călătorie, mașina a fost mereu alături de tine.</h3>
+                    <h3>Respect-o și pregătește-o pentru noi aventuri la <span className="hashtag-alpha-auto">#AlphaAuto</span></h3>
                   </div>
+                </div>
                 <div className="col col-lg-12 col-md-12">
                   <div className="desktop-buttons-container">
                     <div className="large-screen-only desktop-button btns">
@@ -25,16 +26,25 @@ class SimpleSlider extends Component {
                       <AnchorLink offset='27' href='#appointment' className="theme-btn-s2">Programări</AnchorLink>
                     </div>
                   </div>
-                  <div className="mobile-buttons-container">
-                    <div className="mobile-only localisation-button">
-                      <AnchorLink offset='20' href='#contact' className="theme-btn-s2">Contact</AnchorLink>
-                    </div>
-                    <div className="mobile-only call-now-button">
-                      <AnchorLink offset='10' href='#appointment' className="theme-btn-s2">Programări</AnchorLink>
-                    </div>
+                </div>
+              </div>
+
+            <div className="row hero-container mobile-only">
+              <div className="col col-lg-9 col-md-7 slide-caption mobile-only">
+                <div className="slide-title mobile-only">
+                  <h5>Indiferent de călătorie, mașina a fost mereu alături de tine.</h5>
+                  <h5>Respect-o și pregătește-o pentru noi aventuri la <span className="hashtag-alpha-auto">#AlphaAuto</span></h5>
+                </div>
+                <div className="mobile-buttons-container">
+                  <div className="mobile-only localisation-button">
+                    <AnchorLink offset='20' href='#contact' className="theme-btn-s2">Contact</AnchorLink>
+                  </div>
+                  <div className="mobile-only call-now-button">
+                    <AnchorLink offset='10' href='#appointment' className="theme-btn-s2">Programări</AnchorLink>
                   </div>
                 </div>
               </div>
+            </div>
           </div>
       </section>
     );
