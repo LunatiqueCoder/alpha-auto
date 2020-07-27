db7c74fc4f1c506cef8474a55b6e6e3f12844f02
Alexandru Andronache <alexandru.andronache@iquestgroup.com>
2020-07-27 03:28:27 +0300

    beta improvements - move file / remove unused png

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Homepage from "../components/HomePage";
+import Homepage from "@components/HomePage";
 
 const AllRoute = () => {
   return (
